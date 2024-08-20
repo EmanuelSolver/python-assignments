8b2cc6d3456a14eca47488c21297239c9f7f5895
Emanuel Solver <emanuelmutuiri@gmail.com>
2024-08-20 15:51:27 +0300

    created user_input file

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# input name
+name = input("What is your name? ")
+
+# user age
+age = int(input("\nAdd you age: "))  # Convert the input to an integer
+
+# location of the user
+location = input("\nWhere do you live? ")
+
+# display personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
